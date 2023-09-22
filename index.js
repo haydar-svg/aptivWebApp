4c86824d55ad2802a2935747e69f2aa3a9481579
mostafa haydar <haydarmustapha6@gmail.com>
2023-09-22 19:41:55 +0200

    maybe i finish this project

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
-function avoidRepetation(id, dpn, type_0, type_1, type_2, type_3, type_4, type_5, type_6, type_7, type_8, type_9, type_10) {
+function avoidRepetation(id, dpn, t_0, t_1, t_2, t_3, t_4, t_5, t_6, t_7, t_8, t_9, t_10) {
     document.getElementById("dataContainer").innerHTML += `
-    <div class="infoLine">
-            <div class="cell_1">
-                ${id}
+    <div class="infoLine ">
+            <div class="cell_1 lazy-load-div" data-content="${id}">
             </div>
-            <div class="cell_2">
-                ${dpn}
+            <div class="cell_2 lazy-load-div" data-content="${dpn}">
             </div>
-            <div class="cell_3" style=" background-color: #c3deff33;">
+            <div class="cell_3 CB">
                 <div class="checkTable">
-                    <div class="imgContainer">
-                        ${type_0}
-                        </div>
-                 <div class="imgContainer">
-                       ${type_1}
-                        </div>
-                    <div class="imgContainer">
-                       ${type_2}
-                        </div>
+                    <div >
+                        ${t_0}
+                    </div>
+                    <div >
+                       ${t_1}
+                    </div>
+                    <div >
+                       ${t_2}
+                    </div>
                 </div>
             </div>
             <div class="cell_4">
                 <div class="checkTable">
-                    <div class="imgContainer">
-                       ${type_3}</div>
-                    <div class="imgContainer">
-                        ${type_4}
+                    <div >
+                       ${t_3}</div>
+                    <div >
+                        ${t_4}
                     </div>
-                    <div class="imgContainer">
-                       ${type_5}</div>
+                    <div >
+                       ${t_5}</div>
                 </div>
             </div>
-            <div class="cell_5" style=" background-color: #c3deff33;">
+            <div class="cell_5 CB">
                 <div class="checkTable">
-                    <div class="imgContainer">
-                        ${type_6}
+                    <div >
+                        ${t_6}
                     </div>
-                    <div class="imgContainer">
-                       ${type_7}
+                    <div >
+                       ${t_7}
                        </div>
                 </div>
             </div>
             <div class="cell_6">
                 <div class="checkTable">
-                    <div class="imgContainer">
-                       ${type_8}
+                    <div >
+                       ${t_8}
                     </div>
                 </div>
             </div>
-            <div class="cell_7" style=" background-color: #c3deff33;">
+            <div class="cell_7 CB">
                 <div class="checkTable">
-                    <div class="imgContainer">
-                       ${type_9}
+                    <div >
+                       ${t_9}
                        </div>
-                    <div class="imgContainer">
-                       ${type_10}
+                    <div >
+                       ${t_10}
                        </div>
                 </div>
             </div>
